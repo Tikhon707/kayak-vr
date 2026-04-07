539bbcaf88fc4ed3cf893e63e8f1d3eca69b9216
ne4 <dnechaev@corp.local>
2026-04-07 22:35:51 +0500

    Added gay hub, pause panel, pause button

---
+++ KayakUnity/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    [Header("Настройки")]
+    [SerializeField] private string sceneName;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        SceneManager.LoadScene(sceneName);
+    }
+}
